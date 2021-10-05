b9fbeb54440cb6c7282808bd7681b72f13e9e29b
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2021-10-06 00:00:24 +0700

    index update

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ include_once("include/config.inc.php");
           <div class="container-xl">
             <ul class="navbar-nav">
               <li class="nav-item active">
-                <a class="nav-link" href="./index.html">
+                <a class="nav-link" href="./index.php">
                   <span class="nav-link-icon d-md-none d-lg-inline-block">
                     <!-- Download SVG icon from http://tabler-icons.io/i/home -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
